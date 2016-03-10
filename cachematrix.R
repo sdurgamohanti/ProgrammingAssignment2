6750353b7e066aa367bf345b6c45b93a91c2677b
sdurgamohanti <Srinivas.Durgamohanti@gmail.com>
2016-03-10 07:04:56 -0500

    CacheMatrix.R changes
    Pushing cacheMatrix.R changes to remote repository

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+#makeCacheMatrix() : returns a list containing accessor methods to set and get the matrix and its inverse from cache
+#cachSolve(): tries to get the inverse from cache, if not present computes the inverse, sets in cache and returns the inverse matrix.
 
-## Write a short comment describing this function
 
+#makeCacgeMatrix takes a matrix input
+      #setter getter methods are defined to set(reset) and access the matrix
+      #setter getter methods are defined to set and access the inverse of the matrix input.
 makeCacheMatrix <- function(x = matrix()) {
-
+      #holder for the inverse
+      m <- NULL
+      # setter function to set a new matrix and reset the inverse.
+      setMatrix <- function(y) {
+            x <<- y
+            m <<- NULL
+      }
+      getMatrix <- function() x
+      setInverseInCache <- function(inverse) m <<- inverse
+      getInverseInCache <- function() m
+      list(set = setMatrix, get = getMatrix,
+           setInverseInCache = setInverseInCache,
+           getInverseInCache = getInverseInCache)
 }
 
 
-## Write a short comment describing this function
+#cachSolve(): tries to get the inverse from cache, if not present computes the inverse, sets in cache and returns the inverse matrix.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      ## Return a matrix that is the inverse of 'x'
+      m <- x$getInverseInCache()
+      if(!is.null(m)) {
+            message("getting cached data")
+            return(m)
+      }
+      data <- x$get();
+      m <- solve(data)
+      x$setInverseInCache(m)
+      m
 }
